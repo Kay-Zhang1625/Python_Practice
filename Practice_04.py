ff4cfd2fd66327e355ab72b0f7e2ddcb9b58a001
Kay Zhang <kayforeveronly@gmail.com>
2020-12-16 19:14:28 +0800

    Scrape product detail info but suffer in a nonetype item.

--- Practice_04.py
+++ Practice_04.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getData(link):
+def top100Page(link):
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
     resp = requests.post(link, headers=headers)
     soup = BeautifulSoup(resp.text, 'html.parser')
@@ -11,12 +11,12 @@ def getData(link):
     return items
 
 
-def saveData(items):
+def top100(items):
     row_list = []
     for item in items:
         rank = item.select('.zg-badge-text')[0].text
         item_name = item.select('div.p13n-sc-truncate-desktop-type2')[0].text.strip()
-        item_link = 'https://www.amazon.com/' + item.find('a', href=True).get('href')
+        item_link = 'https://www.amazon.com' + item.find('a', href=True).get('href')
         img_src = item.find('img')['src']
     
         data = {}
@@ -30,6 +30,35 @@ def saveData(items):
     return row_list
 
 
+def productDetail(link):
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    resp = requests.post(link, headers=headers)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    
+    store = soup.select_one('a#bylineInfo').text
+    store_link = 'https://www.amazon.com' + soup.find('a', id='bylineInfo', href=True).get('href')
+    price = soup.select_one('span#priceblock_saleprice').text
+    star = soup.select_one('span.a-size-medium.a-color-base').text
+    # reviews_mentioned_s = soup.select('div.a-fixed-left-grid-col.a-col-right').find_all('div.#cr-dp-desktop-lighthut')
+    # reviews_mentioned = []
+    # for i in reviews_mentioned_s:
+    #     reviews = i.select('a span')
+    #     reviews_mentioned.append(reviews)
+
+    data = {}
+    data['store'] = store
+    data['store_link'] = store_link
+    data['price'] = price
+    data['star'] = star
+    # data['reviews_mentioned'] = reviews_mentioned[0]
+
+    return data
+
+
+# def getDetailInfo():
+
+
+
 def writeCSVfile(row_list):
     with open('/Users/kayzhang/Github/Public/Python_Practice/brushes.csv', 'w') as f:
         headers = ['rank', 'item_name', 'item_link', 'img_src']
@@ -39,11 +68,13 @@ def writeCSVfile(row_list):
 
 
 if __name__ == "__main__":
-    page1 = 'https://www.amazon.com/Best-Sellers-Beauty-Makeup-Brush-Sets-Kits/zgbs/beauty/11059451/ref=zg_bs_pg_2?_encoding=UTF8&pg=1'
-    page1_data = saveData(getData(page1))
-    page2 = 'https://www.amazon.com/Best-Sellers-Beauty-Makeup-Brush-Sets-Kits/zgbs/beauty/11059451/ref=zg_bs_pg_2?_encoding=UTF8&pg=2'
-    page2_data = saveData(getData(page2))
+    # page1 = 'https://www.amazon.com/Best-Sellers-Beauty-Makeup-Brush-Sets-Kits/zgbs/beauty/11059451/ref=zg_bs_pg_2?_encoding=UTF8&pg=1'
+    # page1_data = top100(top100Page(page1))
+    # page2 = 'https://www.amazon.com/Best-Sellers-Beauty-Makeup-Brush-Sets-Kits/zgbs/beauty/11059451/ref=zg_bs_pg_2?_encoding=UTF8&pg=2'
+    # page2_data = top100(top100Page(page2))
     
-    page1_data.extend(page2_data)
+    # page1_data.extend(page2_data)
 
-    writeCSVfile(page1_data)
+    # writeCSVfile(page1_data)
+    url = 'https://www.amazon.com/BS-MALL-Brushes-Synthetic-Foundation-Concealers/dp/B01LZ3RLPC/ref=zg_bs_11059451_1?_encoding=UTF8&psc=1&refRID=JSS5HDZRQG2QGN8DYZ2E'
+    print(productDetail(url))
